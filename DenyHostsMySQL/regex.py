a35bfd1aecbd7e90f3c92b4feef9abce6c12d1a8
Jose Vargas <jose.vargas@tmmdata.com>
2017-04-24 17:58:29 -0400

    removing regexes that aren't needed and putting the correct format for parsing mysql log messages

--- DenyHostsMySQL/regex.py
+++ DenyHostsMySQL/regex.py
@@ -6,34 +6,16 @@ import re
 
 #DATE_FORMAT_REGEX = re.compile(r"""(?P<month>[A-z]{3,3})\s*(?P<day>\d+)""")
 
-MYSQLD_FORMAT_REGEX = re.compile(r""".* (mysqld.*:|\[mysqld\]) (?P<message>.*)""")
-#MYSQLD_FORMAT_REGEX = re.compile(r""".* mysqld.*: (?P<message>.*)""")
+MYSQLD_FORMAT_REGEX = re.compile(r""".* (\[Note\]) (?P<message>.*)""")
 
-FAILED_ENTRY_REGEX = re.compile(r"""Failed (?P<method>\S*) for (?P<invalid>invalid user |illegal user )?(?P<user>.*) from (::ffff:)?(?P<host>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})( port \d+)?( mysql)?$""")
-
-FAILED_ENTRY_REGEX2 = re.compile(r"""(?P<invalid>(Illegal|Invalid)) user (?P<user>.*) from (::ffff:)?(?P<host>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})$""")
-
-FAILED_ENTRY_REGEX3 = re.compile(r"""Authentication failure for (?P<user>.*) .*from (::ffff:)?(?P<host>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})""")
-
-FAILED_ENTRY_REGEX4 = re.compile(r"""Authentication failure for (?P<user>.*) .*from (?P<host>.*)""")
-
-FAILED_ENTRY_REGEX5 = re.compile(r"""User (?P<user>.*) .*from (?P<host>.*) not allowed because none of user's groups are listed in AllowGroups$""")
-
-FAILED_ENTRY_REGEX6 = re.compile(r"""Did not receive identification string .*from (::ffff:)?(?P<host>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})""")
-
-FAILED_ENTRY_REGEX7 = re.compile(r"""User (?P<user>.*) .*from (::ffff:)?(?P<host>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) not allowed because not listed in AllowUsers""")
-
-FAILED_ENTRY_REGEX8 = re.compile(r"""authentication error for (?P<user>.*) .*from (?P<host>.*)""")
-FAILED_ENTRY_REGEX9 = re.compile(r"""Invalid user (?P<user>.*) .*from (::ffff:)?(?P<host>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})""")
-
-
-FAILED_ENTRY_REGEX10 = re.compile(r"""Connection closed by (?P<host>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) \[preauth\]""")
+#Access denied for user 'username'@'host' (using password: NO|YES)
+FAILED_ENTRY_REGEX = re.compile(r"""[Aces\sdni\sforu]+'(?P<user>.*)['@]{3}(?P<host>.*)'[\s\(usingpaword:\sYESNO)]+""")
 
 # these are reserved for future versions
-FAILED_ENTRY_REGEX11 = None
+FAILED_ENTRY_REGEX2 = None
 
 # this should match the highest num failed_entry_regex + 1
-FAILED_ENTRY_REGEX_NUM = 11
+FAILED_ENTRY_REGEX_NUM = 2
 
 FAILED_ENTRY_REGEX_RANGE = list(range(1, FAILED_ENTRY_REGEX_NUM))
 FAILED_ENTRY_REGEX_MAP = {}
@@ -44,9 +26,6 @@ for i in FAILED_ENTRY_REGEX_RANGE:
     else: extra = "%i" % i
     rx = eval("FAILED_ENTRY_REGEX%s" % extra)
     FAILED_ENTRY_REGEX_MAP[i] = rx
-
-
-SUCCESSFUL_ENTRY_REGEX = re.compile(r"""Accepted (?P<method>\S+) for (?P<user>.*) from (::ffff:)?(?P<host>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})( port \d+)?( mysql)?$""")
 
 TIME_SPEC_REGEX = re.compile(r"""(?P<units>\d*)\s*(?P<period>[smhdwy])?""")
 
